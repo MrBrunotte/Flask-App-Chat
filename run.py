b3a73243e59316555543454f1fa3fd807c02c800
mrbrunotte <mrbrunotte@gmail.com>
2019-08-02 12:59:34 +0200

    Add the abbility to read and write from a list

--- run.py
+++ run.py
@@ -1,27 +1,40 @@
 import os                       # import os to have access to the environment variables
-from flask import Flask         # import Flask
+from flask import Flask, redirect         # import Flask and redirect
 
 app = Flask(__name__)           # initialize the new flask app
+messages = []                   # create an empty list called messages
 
 
-@app.route('/')                 # create the app route decorator.
-# define the function  that is going to bound to our decorator.
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
+
+
+@app.route("/")
 def index():
-    """ Main page instructions """
-    return "To send a message /USERNAME/MESSAGE"
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
 
 
-@app.route('/<username>')
+@app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
-@app.route('/<username>/<message>')
+
+@app.route("/<username>/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username,message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
+
 
 #app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
-
-
 if __name__ == '__main__':
     # NEVER HAVE DEBUG=TRUE IN PRODUCTION OR WHEN SUBMITTING!!!
     app.run(debug=True)
